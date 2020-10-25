04f351bdfff19e1b90882db34652b00bb591f287
Marcelo R. Silva <marcelo.rosario.silva@gmail.com>
2020-10-24 22:20:10 -0300

    Ajuste desafio3

--- Introducao_Programacao/desafio3/index.js
+++ Introducao_Programacao/desafio3/index.js
@@ -1,3 +1,4 @@
+const gets = [1,1,1,1,1];
 const gramasPessoas = [300, 1500, 600, 1000, 150];
 let arr = []
 let limit = gramasPessoas.length;
@@ -6,7 +7,7 @@ for(i=0; i < limit; i++){
 }
 
 let r1 = arr.map((value, index) => {
-    return arr[index] = value * gramasPessoas[index];
+    return value * gramasPessoas[index];
 })
 
 let r2 = r1.reduce((previous, current, index, array) => {
